7e3bbba9d738bc96fb70a52834e3156f5798013c
Quonnaq0711 <shaquonna.lynna@gmail.com>
2024-10-14 12:57:40 -0400

    <AuthMeComplete>

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -4,7 +4,7 @@ import { FaUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
 import OpenModalMenuItem from './OpenModalMenuItem';
 import LoginFormModal from '../LoginFormModal';
-import SignupFormModal from '../SignupFormModal';
+import SignupFormModal from '../SignUpFormPageModal';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
